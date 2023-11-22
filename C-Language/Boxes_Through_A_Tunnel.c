bb1813890e6e783c447e55d4ccb9603cb24ad253
Nishant Gaurav <140972654+nishant05gaurav@users.noreply.github.com>
2023-11-22 17:41:44 +0530

    first commit

---
+++ C-Language/Boxes_Through_A_Tunnel.c
@@ -0,0 +1,25 @@
+// Question : https://www.hackerrank.com/challenges/too-high-boxes/problem?isFullScreen=true
+
+// Solution:
+
+
+
+struct box
+{
+	int length, width, height;
+};
+
+typedef struct box box;
+
+int get_volume(box b) {
+	return (b.length * b.width * b.height);
+}
+
+int is_lower_than_max_height(box b) {
+	if(b.height < MAX_HEIGHT)
+        return 1;
+    else 
+        return 0;
+    
+}
+

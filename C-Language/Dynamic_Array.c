c52d9bc9c1ac511a0d19986f4a0a3f0a36addd29
Nishant Gaurav <140972654+nishant05gaurav@users.noreply.github.com>
2023-11-28 22:58:03 +0530

    first commit

---
+++ C-Language/Dynamic_Array.c
@@ -0,0 +1,26 @@
+// Question:    https://www.hackerrank.com/challenges/frequency-of-digits-1/problem?isFullScreen=true
+
+// Solution: 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    
+ 
+    char s[1001];
+    scanf("%[^\n]%*c", s);
+    
+    int a[10] = {0};
+    for(int i = 0; i < strlen(s); i++){
+        if(s[i] >= '0' && s[i] <= '9')
+            a[s[i] - '0']++;
+    }
+    for(int i = 0; i < 10; i++)
+    
+     printf("%d ", a[i]);
+        
+    printf("\n");
+    return 0;
+}

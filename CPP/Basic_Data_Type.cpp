21df764eff32f27148768523c787497ec4944f16
Nishant Gaurav <140972654+nishant05gaurav@users.noreply.github.com>
2024-06-12 21:42:40 +0530

    first commit

---
+++ CPP/Basic_Data_Type.cpp
@@ -0,0 +1,23 @@
+// Question: https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    cin >> a >> b >> c >> d >> e;
+    
+    printf("%d\n%ld\n%c\n%f\n%lf\n", a, b, c, d, e);
+    return 0;
+
+    
+    return 0;
+}
